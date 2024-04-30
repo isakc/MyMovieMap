973dd7ccaf77fe30b6883122c0b15e73b2def6d7
lsakc15 <ckals15@naver.com>
2024-04-30 21:31:22 +0900

    Update: 2024-04-30: 배포하기 위해서 수정

--- src/main/java/com/model2/mvc/MyMovieMapApplication.java
+++ src/main/java/com/model2/mvc/MyMovieMapApplication.java
@@ -2,12 +2,18 @@ package com.model2.mvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MyMovieMapApplication {
+public class MyMovieMapApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyMovieMapApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(MyMovieMapApplication.class);
+	}
 }
