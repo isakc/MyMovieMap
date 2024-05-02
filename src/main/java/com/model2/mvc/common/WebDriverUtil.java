4353b1aa0af4161d56ace5f6db6dd7c850ae6f75
lsakc15 <ckals15@naver.com>
2024-05-02 21:15:51 +0900

    Update: 2024-05-02: 셀레니움 파일에 추가

--- src/main/java/com/model2/mvc/common/WebDriverUtil.java
+++ src/main/java/com/model2/mvc/common/WebDriverUtil.java
@@ -1,9 +1,9 @@
 package com.model2.mvc.common;
 
 import java.time.Duration;
-//import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.chrome.ChromeDriver;
-//import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.util.ObjectUtils;
@@ -11,43 +11,43 @@ import org.springframework.util.ObjectUtils;
 @Component
 public class WebDriverUtil {
 
-//    private static String WEB_DRIVER_PATH; // WebDriver 경로
-//
-//    public static WebDriver getChromeDriver() {
-//        if (ObjectUtils.isEmpty(System.getProperty("webdriver.chrome.driver"))) {
-//            System.setProperty("webdriver.chrome.driver", WEB_DRIVER_PATH);
-//        }
-//
-//        // webDriver 옵션 설정
-//        ChromeOptions chromeOptions = new ChromeOptions();
-//        chromeOptions.setHeadless(true);
-//        chromeOptions.addArguments("--lang=ko");
-//        chromeOptions.addArguments("--no-sandbox");
-//        chromeOptions.addArguments("--disable-dev-shm-usage");
-//        chromeOptions.addArguments("--disable-gpu");
-//        chromeOptions.addArguments("--remote-allow-origins=*");
-//        chromeOptions.setCapability("ignoreProtectedModeSettings", true);
-//
-//        WebDriver driver = new ChromeDriver(chromeOptions);
-//        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
-//
-//        return driver;
-//    }
-//
-//	@Value("${common.chromeDriverPath}")
-//    public void initDriver(String path) {
-//        WEB_DRIVER_PATH = path;
-//    }
-//
-//    public static void quit(WebDriver driver) {
-//        if (!ObjectUtils.isEmpty(driver)) {
-//            driver.quit();
-//        }
-//    }
-//
-//    public static void close(WebDriver driver) {
-//        if (!ObjectUtils.isEmpty(driver)) {
-//            driver.close();
-//        }
-//    }
+    private static String WEB_DRIVER_PATH; // WebDriver 경로
+
+    public static WebDriver getChromeDriver() {
+        if (ObjectUtils.isEmpty(System.getProperty("webdriver.chrome.driver"))) {
+            System.setProperty("webdriver.chrome.driver", WEB_DRIVER_PATH);
+        }
+
+        // webDriver 옵션 설정
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.setHeadless(true);
+        chromeOptions.addArguments("--lang=ko");
+        chromeOptions.addArguments("--no-sandbox");
+        chromeOptions.addArguments("--disable-dev-shm-usage");
+        chromeOptions.addArguments("--disable-gpu");
+        chromeOptions.addArguments("--remote-allow-origins=*");
+        chromeOptions.setCapability("ignoreProtectedModeSettings", true);
+
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+
+        return driver;
+    }
+
+	@Value("${common.chromeDriverPath}")
+    public void initDriver(String path) {
+        WEB_DRIVER_PATH = path;
+    }
+
+    public static void quit(WebDriver driver) {
+        if (!ObjectUtils.isEmpty(driver)) {
+            driver.quit();
+        }
+    }
+
+    public static void close(WebDriver driver) {
+        if (!ObjectUtils.isEmpty(driver)) {
+            driver.close();
+        }
+    }
 }
